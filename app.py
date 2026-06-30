6fe3422aae2b56f1488aaa9cfc382d79a061e62f
Ubuntu <ubuntu@ip-172-31-21-124.ap-northeast-1.compute.internal>
2026-06-30 07:43:50 +0000

    Modify App Message

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 def hello():
     return '''
     <h1>🎉 자동 배포 테스트 (EC2 from)</h1>
-    <p>이 앱은 EC2의 Docker 컨테이너에서 실행 중입니다!</p>
-    <p>당신은 비전문가에서 DevOps 엔지니어로 변신 중 🚀</p>
+    <p>🚀 이 앱은 EC2의 Docker 컨테이너에서 실행 중입니다!🚀 </p>
     '''
 
 if __name__ == '__main__':
